eea7c3a8d51ff96a5c425059c7238811a1ea88b1
Ariel Squatrito <squatritoariel@gmail.com>
2023-12-19 18:00:07 -0300

    Contador de clicks con React

---
+++ src/contadorApp.jsx
@@ -0,0 +1,18 @@
+
+export const ContadorApp = ({ value }) => {
+    
+    const handleClick = () => {
+        value += 1
+        console.log(value)
+    }
+
+    return (
+        <>
+            <h1>Contador: </h1>
+            <p>{value}</p>
+            <button onClick={handleClick}>
+                Soy un botón
+            </button>
+        </>
+    )
+}
