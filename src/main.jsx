8dd983a3fb608e18ebf15fcca924865dab350f01
Ariel Squatrito <squatritoariel@gmail.com>
2023-12-27 13:27:37 -0300

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import { ListadoApp } from './ListadoApp'
+import { UserApp } from './components/UserApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ListadoApp/>
+    <UserApp />
   </React.StrictMode>,
 )
