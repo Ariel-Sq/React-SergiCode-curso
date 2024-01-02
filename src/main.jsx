ac0b99cca03260736b15264669e08fbc550667c1
Ariel Squatrito <squatritoariel@gmail.com>
2024-01-02 13:51:35 -0300

    customHooks, helpers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import { UserApp } from './components/UserApp'
+import { UserApp } from './UserApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserApp />
+    <UserApp/>
   </React.StrictMode>,
 )
